10b86ec9b8149ddcc29d4c55791298850a142fc5
frankipod <yaoxiaochu@gmail.com>
2016-04-04 17:03:35 -0500

    Update tree_maxdepth.py

--- tree_maxdepth.py
+++ tree_maxdepth.py
@@ -15,32 +15,45 @@ class Tree:
         return str(self.cargo)
         
     def prtlfs(self,root):
-        if root:
-            if root.left and root.right:
-                print '     ', root.cargo
-                print root.left.cargo,'         ',     root.right.cargo,'\n'
-                self.prtlfs(root.left)
-                self.prtlfs(root.right)
+        if not root:
+            return
+            #print '     ', root.cargo
+        if root.left and not root.right:
+            print (root.cargo,root.left.cargo,'null'),
+            self.prtlfs(root.left)
+            self.prtlfs(root.right)
+        elif root.right and not root.left:
+           
+            print (root.cargo,'null',root.right.cargo), 
+            self.prtlfs(root.left)
+            self.prtlfs(root.right)
+        elif root.right and  root.left:
+            print (root.cargo,root.left.cargo,root.right.cargo), 
+            self.prtlfs(root.left)
+            self.prtlfs(root.right)
+        else:
+            print (root.cargo,'null','null'),
+            self.prtlfs(root.left)
+            self.prtlfs(root.right)
         return
 
-count = 1
 class Solution(object):
     def maxDepth(self,root): 
-        global count
-        if root:
-              
-            if root.left:
-                self.maxDepth(root.left)
-                count += 1
-                if root.right:
-                    self.maxDepth(root.right)
-                    count += 1
-        return count
+        
+        def dfs(lvl,root,res):        
+            if root:
+                if lvl > res[0]:
+                    res[0] = lvl                            
+                dfs(lvl+1,root.left,res)
+                dfs(lvl+1,root.right,res)
+        res = [0]
+        dfs(1,root,res)
+                #print '     ', root.cargo11            
+        return res[0] 
 
 
-
-
-a = Tree(1,Tree(2,None ,None),Tree(3, None,Tree(5)))
+a = Tree(1,Tree(2,None ,None),Tree(3, None,Tree(5,Tree(7),None)))
+#a = Tree(1,Tree(2),Tree(3))
 a.prtlfs(a)
 b = Solution()
 depth = b.maxDepth(a)
