42c540a55ad5a6d79cb506be02eeea628f7c4beb
frankipod <yaoxiaochu@gmail.com>
2016-04-06 17:16:50 -0500

    Added files via upload

---
+++ linklst.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 06 13:37:56 2016
+
+@author: yaoxia
+"""
+
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+def CrtList(head):
+    if not head:
+        lst = []        
+        return lst
+    lst = []
+    def crtlist(head,lst):
+        if head :
+            lst.append(head.val) 
+            crtlist(head.next,lst)
+        return lst
+    return crtlist(head,lst)
+    
+    
+def CreatLinkList(nums):
+    if not nums:
+        a = None
+        return a
+    lst = []
+    for num in nums:
+        lst.append(ListNode(num))    
+    for i in range(len(lst[:-1])):
+        lst[i].next = lst[i+1] 
+    a = lst[0]
+    return a
+    
+def rmListVal(lst,val):
+    if all(num != val for num in lst):
+        return lst
+    else:
+        lst.remove(val)
+    lst = rmListVal(lst,val)
+    return lst
+    
